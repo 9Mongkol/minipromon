321ce55f7e4c13e38df6b78c495c1f6df7d83c9b
9Mongkol <9mongkol2005@gmail.com>
2024-08-09 08:52:25 +0700

    02082403

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,41 +1,37 @@
-// app/api/route.js
 import { Client } from 'pg';
 import dotenv from 'dotenv';
 import bcrypt from 'bcrypt';
 
 dotenv.config();
+
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
 });
 
-//------------------------GET---------------------------------
 client.connect();
+
 export async function GET() {
   try {
-        const result = await client.query('SELECT * FROM tbl_users');
-        return new Response(JSON.stringify(result.rows), {
-            status: 200,
-            headers: { "Content-Type": "application/json" },
-        });
+    const result = await client.query('SELECT * FROM tbl_users');
+    return new Response(JSON.stringify(result.rows), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
   } catch (error) {
-    
-        return new Response(JSON.stringify({ error: "Internal Server Error" }), {
-            status: 500,
-            headers: { "Content-Type": "application/json" },
-        });
+    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
   }
 }
-//------------------------END GET---------------------------------
-
-//--------------------------POST---------------------------
-
+//-------------------------------------------------------------------------------------
 export async function POST(request) {
   try {
   const { firstname, lastname, username, password } = await request.json();
   // Hash password
   const hashedPassword = await bcrypt.hash(password, 10);
-  console.log(hashedPassword);
-  const res = await client.query('INSERT INTO tbl_users (firstname, lastname, username, password) VALUES ($1, $2, $3, $4) RETURNING *', [firstname, lastname, username, password]);
+  //console.log(hashedPassword);
+  const res = await client.query('INSERT INTO tbl_users (firstname, lastname, username, password) VALUES ($1, $2, $3, $4) RETURNING *', [firstname, lastname, username, hashedPassword]);
   return new Response(JSON.stringify(res.rows[0]), {
   status: 201,
   headers: { 'Content-Type': 'application/json' },
@@ -48,17 +44,13 @@ export async function POST(request) {
   });
   }
   }
-
-//--------------------------END POST---------------------------
-
-//--------------------------PUT-------------------------------
-
+//-------------------------------------------------------------------------------------
 export async function PUT(request) {
   try {
-  const { id, firstname, lastname,password } = await request.json();
+  const { id, firstname, lastname, password } = await request.json();
   const hashedPassword = await bcrypt.hash(password, 10);
   console.log(hashedPassword);
-  const res = await client.query('UPDATE tbl_users SET firstname = $1, lastname = $2, password = $4 WHERE id = $3 RETURNING *', [firstname, lastname, id, hashedPassword]);
+  const res = await client.query('UPDATE tbl_users SET firstname = $1, lastname = $2, password = $4 WHERE id = $3  RETURNING *', [firstname, lastname, id, hashedPassword]);
   if (res.rows.length === 0) {
   return new Response(JSON.stringify({ error: 'User not found' }), {
   status: 404,
@@ -77,11 +69,7 @@ export async function PUT(request) {
   });
   }
   }
-
-//--------------------------END PUT-------------------------------
-
-//--------------------------DEL-------------------------------
-
+//-------------------------------------------------------------------------------------
 export async function DELETE(request) {
   try {
   const { id } = await request.json();
@@ -104,5 +92,4 @@ export async function DELETE(request) {
   });
   }
   }
-
-//--------------------------DEL-------------------------------
+//------------------------------------------------------------
